a8b95570890a8061191a1dc8facbae1e09c8b340
Carolina Herrera <carolinaherrera72@gmail.com>
2026-01-07 19:18:19 +0000

    ok2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <--- Agrega esta línea
 };
-
 export default nextConfig;
